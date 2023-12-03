848428237aea760aa030e110eda11efaf4fddc19
kh <khhy33@naver.com>
2023-12-03 23:40:10 +0900

    내용 수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,11 +4,12 @@ const Intro = () => {
   return (
     <section id="intro">
       <div className="intro__inner">
+        <span className="notice">크롬브라우저(PC버전)를 권장합니다.</span>
         <h2 className="intro__title">Kim's Portpolio</h2>
         <div className="intro__skill">
           <span className="intro__skill">사용 프로그램 및 스킬</span>
           <span className="intro__txt">Photoshop,  Illustrator, Dreamweaver, VSCode, MS Office<br />
-HTML, CSS, SCSS, UI, Javascript, React JS, Jquery</span>
+UI/UX Design, HTML, CSS, SCSS, UI, Javascript, React JS, Jquery</span>
         </div>
       </div>
     </section>
